3fdb4f928f913f1b2864e8ba7b7cd1035c96fe33
Omri Ruvio <omriruvio@gmail.com>
2022-08-12 01:53:01 +0300

    types: parseCropData

--- utils/parseCropData.ts
+++ utils/parseCropData.ts
@@ -1,15 +1,35 @@
-export const parseCropData = (messageHistory: any): any => {
+export interface UserMessage {
+  dateReceived: string;
+  messageBody: string;
+  temperature?: string | null;
+  humidity?: string | null;
+  ph?: string | null;
+  ec?: string | null;
+  handled: boolean;
+  _id: string;
+  imageUrl?: string | null;
+}
+
+export interface ParsedTableData {
+  labels: string[];
+  temperatures: string[];
+  humidities: string[];
+  phs: string[];
+  ecs: string[];
+}
+
+export const parseCropData = (messageHistory: UserMessage[]): ParsedTableData => {
   const data = messageHistory.reduce(
-    (data: any, message: any) => {
+    (data: ParsedTableData, message) => {
       const { humidity, ph, ec, temperature } = message;
       // formats date variable to 'DD/MM'
       const date = new Date(message.dateReceived).toLocaleString().split(',')[0].slice(0, -5);
       if ([humidity, ph, ec, temperature].some((field) => Boolean(field))) {
         data.labels.push(date);
-        data.temperatures.push(temperature || 0);
-        data.ecs.push(ec || 0);
-        data.phs.push(ph || 0);
-        data.humidities.push(humidity || 0);
+        data.temperatures.push(temperature || '0');
+        data.ecs.push(ec || '0');
+        data.phs.push(ph || '0');
+        data.humidities.push(humidity || '0');
       }
       return data;
     },
