4194a3e0fec0f90071f1f5de395f7c43d5ba50db
Omri Ruvio <omriruvio@gmail.com>
2022-09-11 22:47:59 +0300

    refactor: update prod url

--- config.ts
+++ config.ts
@@ -1,8 +1,6 @@
 // export const BASE_URL = 'https://39ad-176-228-28-109.eu.ngrok.io';
 // prettier-ignore
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_MAIN_API_URL_DEV ||
-  'https://hydroponicsil.herokuapp.com';
+export const BASE_URL = process.env.NEXT_PUBLIC_MAIN_API_URL_DEV || process.env.NEXT_PUBLIC_MAIN_API_URL_PROD;
 
 export const NORMAL_DATA_RANGE_METRIC = {
   EC: { MIN: 700, MAX: 3000 },
